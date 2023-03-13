fd5c120d084777a22b56a5fe977941f5d8847d25
Salajan Bogdan <salajan.bogdan_a@yahoo.com>
2023-03-13 14:56:48 +0200

    Add TeamsTable

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import './App.css';
+import { TeamsTable } from "./TeamsTable";
+
+let teams = [
+  {
+    "name": "CV",
+    "members": "Salajan Bogdan",
+    "url": "https://github.com/sljbogdan",
+  },
+  {
+    "name": "CV",
+    "members": "Bonat Paula",
+    "url": ""
+  }
+]
 
 function App() {
   return (
     <div>
       <h1>Teams Networking</h1>
       <div>Search</div>
-      <p>table...</p>
+      <TeamsTable teams={teams} />
     </div>
   );
 }
---
+++ src/TeamsTable.js
@@ -0,0 +1,33 @@
+export const TeamsTable = (props) => (
+    <table border="1">
+      <thead>
+        <tr>
+          <th>Members</th>
+          <th>Project Name</th>
+          <th>Project URL</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        {props.teams.map((team, index) => (
+          <tr key={index}>
+            <td>{team.members.split(/\s*,\s*/).join("<br>")}</td>
+            <td>{team.name}</td>
+            <td><a target="_blank" href={team.url}>Github</a></td>
+            <td>
+              <a href="#" className="delete-row" data-id="{team.id}">&#10006;</a>
+              <a href="#" className="edit-row" data-id="{team.id}">&#9998;</a>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+      <tfoot>
+        <tr>
+          <td><input type="text" name="members" placeholder="enter Members" /></td>
+          <td><input type="text" name="name" placeholder="enter Project name" /></td>
+          <td><input type="text" name="url" placeholder="enter Project URL" /></td>
+          <td><button>Save</button></td>
+        </tr>
+      </tfoot>
+    </table>
+  );

3e7968e506948790dbb276e6b67d49775c462972
Salajan Bogdan <salajan.bogdan_a@yahoo.com>
2023-03-15 12:47:54 +0200

    add Component as class + internal state

--- src/App.js
+++ src/App.js
@@ -1,27 +1,48 @@
+import { Component } from 'react';
 import './App.css';
 import { TeamsTable } from "./TeamsTable";
 
-let teams = [
-  {
-    "name": "CV",
-    "members": "Salajan Bogdan",
-    "url": "https://github.com/sljbogdan",
-  },
-  {
-    "name": "CV",
-    "members": "Bonat Paula",
-    "url": ""
-  }
-]
 
-function App() {
-  return (
-    <div>
-      <h1>Teams Networking</h1>
-      <div>Search</div>
-      <TeamsTable teams={teams} border={2}/>
-    </div>
-  );
+class App extends Component{
+constructor(props) {
+  super(props);
+  this.state = {
+    teams: []
+  }
+}
+
+componentDidMount() {
+  console.warn('mount');
+  setTimeout(()=>{
+    console.warn("loaded");
+    this.setState({
+      teams: [
+        {
+          "name": "CV",
+          "members": "Salajan Bogdan",
+          "url": "https://github.com/sljbogdan",
+        },
+        {
+          "name": "CV",
+          "members": "Bonat Paula",
+          "url": ""
+        }
+      ]
+    })
+  }, 2000)
+  
+}
+
+  render (){
+    console.debug(this.state.teams);
+    return (
+      <div>
+        <h1>Teams Networking</h1>
+        <div>Search</div>
+        <TeamsTable teams={this.state.teams} border={2}/>
+      </div>
+    );
+  }
 }
 
 export default App;

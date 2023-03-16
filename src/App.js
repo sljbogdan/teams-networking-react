c0360fb61c0b9f3268cffc18c293afc1537d6827
Salajan Bogdan <salajan.bogdan_a@yahoo.com>
2023-03-16 12:19:19 +0200

    move teams in Redux.store

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import { connect } from 'react-redux';
 import './App.css';
 import { TeamsTable } from "./TeamsTable";
 
@@ -9,6 +10,7 @@ class App extends Component{
     this.state = {
       teams: []
     }
+    console.warn("props: ", props);
   }
 
   componentDidMount() {
@@ -16,13 +18,7 @@ class App extends Component{
   }
 
  load(){
-    fetch("http://localhost:3000/teams-json")
-        .then(r => r.json())
-        .then(teams => {
-            this.setState({
-              teams
-            });
-        });
+   
   }
 
   add(team){
@@ -65,13 +61,12 @@ class App extends Component{
 
 
   render (){
-    console.debug(this.state.teams);
     return (
       <div>
         <h1>Teams Networking</h1>
         <div>Search</div>
         <TeamsTable 
-          teams={this.state.teams} 
+          teams={this.props.teams} 
           border={2} 
           onSubmit={team => {
               this.add(team);
@@ -84,5 +79,12 @@ class App extends Component{
     );
   }
 }
+const mapStateToProps = state => {
+  console.info("map state to props", state);
+  return {
+    teams: state.teams
+  }
+};
+const AppContainer = connect(mapStateToProps)(App);
 
-export default App;
+export default AppContainer;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,46 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+const rootReducer = (state = {teams: []}, action) => {
+  console.warn("rootReducer", state, action);
+  switch(action.type) {
+    case 'TEAMS_LOADED': {
+      return {
+        teams: action.teams
+      }
+    }
+    default:
+      return state;
+  }
+   
+  return state;
+};
+
+const store = createStore(rootReducer);
+console.warn("store", store);
+
+store.subscribe(()=>{
+  console.warn("data change", store.getState())
+})
+
+function load(){
+  fetch("http://localhost:3000/teams-json")
+      .then(r => r.json())
+      .then(teams => {
+        store.dispatch({type: 'TEAMS_LOADED', teams});
+          });
+};
+load();
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
